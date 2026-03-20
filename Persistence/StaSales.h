74964bf68fe2989f93730e0ad811dbb7515cba13
szh <2337491585@qq.com>
2026-03-20 09:15:43 +0800

    test

---
+++ Persistence/StaSales.h
@@ -0,0 +1,9 @@
+//
+// Created by shizihao on 2026/3/20.
+//
+
+#ifndef TTMS_STASALES_H
+#define TTMS_STASALES_H
+#include "../Service/StaSales.h"
+int Sale_Perst_SelByID(sale_list_tlist,int usrID);
+#endif //TTMS_STASALES_H
